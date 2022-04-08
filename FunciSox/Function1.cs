f75c605d4b13d19e22ace05a03ab681ca4c98061
Bill Melvin <bill@billmelvin.com>
2022-04-08 14:16:51 -0400

    Extract orchestrator function to new OrchestratorFunctions class.

--- FunciSox/Function1.cs
+++ FunciSox/Function1.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,20 +7,6 @@ namespace FunciSox
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
-        {
-            var outputs = new List<string>();
-
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "London"));
-
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
-        }
 
         [FunctionName("Function1_Hello")]
         public static string SayHello([ActivityTrigger] string name, ILogger log)
---
+++ FunciSox/OrchestratorFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace FunciSox
+{
+    public class OrchestratorFunctions
+    {
+        [FunctionName(nameof(AudioProcessOrchestrator))]
+
+        // TODO: Implement custom return class (custom DTO) for Task<>?
+
+        public static async Task<object> AudioProcessOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var mp3InLocation = context.GetInput<string>();
+
+            var wavInLocation = await context.CallActivityAsync<string>(
+                "ConvertToWav", mp3InLocation);
+
+            var wavOutLocation = await context.CallActivityAsync<string>(
+                "ProcessWav", wavInLocation);
+
+            //var wavFasterLocation = await context.CallActivityAsync<string>(
+            //    "UpTempoWav", wavOutLocation);
+
+            var mp3OutLocation = await context.CallActivityAsync<string>(
+                "ConvertToMp3", wavOutLocation);
+
+            return new
+            {
+                wavIn = wavInLocation,
+                wavOut = wavOutLocation,
+                mp3Out = mp3OutLocation
+            };
+
+        }
+
+    }
+}
+
