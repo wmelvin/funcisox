0f8a5364d7de920f906bbc6d7894b733610b681a
Bill Melvin <bill@billmelvin.com>
2022-04-08 13:53:10 -0400

    Extract starter function to new HttpFunctions class.

--- FunciSox/Function1.cs
+++ FunciSox/Function1.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
@@ -33,18 +31,5 @@ namespace FunciSox
             return $"Hello {name}!";
         }
 
-        [FunctionName("Function1_HttpStart")]
-        public static async Task<HttpResponseMessage> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
-            [DurableClient] IDurableOrchestrationClient starter,
-            ILogger log)
-        {
-            // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("Function1", null);
-
-            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
-
-            return starter.CreateCheckStatusResponse(req, instanceId);
-        }
     }
 }
---
+++ FunciSox/HttpFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FunciSox
+{
+    public static class HttpFunctions
+    {
+        [FunctionName(nameof(HttpStartAudioProcess))]
+        public static async Task<IActionResult> HttpStartAudioProcess(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // TODO: Replace 'AuthorizationLevel.Anonymous'.
+
+            string mp3 = req.GetQueryParameterDictionary()["mp3"];
+            if (mp3 == null)
+            {
+                return new BadRequestObjectResult(
+                    "No mp3 file location specified.");
+            }
+
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync(
+                "AudioProcessOrchestrator", null, mp3);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+
+}
