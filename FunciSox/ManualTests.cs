e6b5f450bb941e8f4ddf29fcd718d47a78e5221c
Bill Melvin <bill@billmelvin.com>
2022-04-28 11:28:26 -0400

    Remove obsolete comment.

--- FunciSox/ManualTests.cs
+++ FunciSox/ManualTests.cs
@@ -60,10 +60,6 @@ namespace FunciSox
 
             await Toolbox.EncodeWavToMp3(wav, mp3Out, tags, log);
 
-            // TODO: Get ID3 tags when doing initial conversion of MP3 to WAV.
-            //log.LogWarning($"Running CopyID3Tags source='{mp3}' target='{mp3Out}'");
-            //await Toolbox.CopyID3Tags(mp3, mp3Out, log);
-
             return new OkResult();
         }
 
